e5688ed33abd2ea77df0572530d09bd0504d7ec7
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2021-07-13 21:06:09 -0300

    13-07-2021
    Iniciado projeto

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme } from "@chakra-ui/react";
+import { transparentize } from "polished";
+
+export const theme = extendTheme({
+  colors: {
+    highlight: "#FFBA08",
+    hightlight50: transparentize(0.5, "#FFBA08"),
+    headingsText: "#47585B",
+    info: "#999999",
+    info50: transparentize(0.5, "#999999"),
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "white",
+        color: "headingsText",
+      },
+    },
+  },
+});
