68dc0d54217ec8ff4b7acde633a14db78dce242b
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2021-07-14 22:10:44 -0300

    14-07-2021

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Flex, Box } from "@chakra-ui/react";
+import logoImg from "../assets/logo.png";
 
+import { useBreakpointValue } from "@chakra-ui/react";
 export default function Home() {
-  return <h1>Teste</h1>;
+  const imageSizes = useBreakpointValue({
+    base: { width: 81, height: 20 },
+    md: {
+      width: 147.25,
+      height: 45.54,
+    },
+  });
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Flex w="100vw" h="100vh">
+        <Flex
+          as="header"
+          justify="center"
+          align="center"
+          flex="1"
+          h={["3.150em", "5em", "5.8em"]}
+        >
+          <Image
+            src={logoImg}
+            width={imageSizes?.width}
+            height={imageSizes?.height}
+          ></Image>
+        </Flex>
+        <Flex flex="1" align="center" justify="space-betwenn"></Flex>
+      </Flex>
+    </>
+  );
 }
